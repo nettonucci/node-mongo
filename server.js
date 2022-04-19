060b0b37805ac9d9537039af6a8735e6c29d3062
José Nucci Netto <nettonucci@gmail.com>
2022-04-19 08:08:11 -0300

    gitignore

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Lista de livros',
+    '/autores': 'Lista de autores',
+    '/sobre': 'Sobre o curso',
+}
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Curso de Node')
+    res.end(rotas[req.url]);
 })
 
 server.listen(port, () => {
